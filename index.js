4103737c1afb0012f3ffa4848e62c8a995a4a75f
VhouAtroph <nataliejlavin@gmail.com>
2021-02-28 19:18:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Bot started.');
+	client.user.setActivity('Bready to react')
+});
+
+client.on('message', message => {
+	if (message.content = ' ') {
+	message.react('🍞');
+	console.log('Bread');
+}
+});
+client.login(insert token here);
